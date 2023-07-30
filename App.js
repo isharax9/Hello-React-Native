0de0a4c31469b515ea29bf8c3f8708d4a4eefc58
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-07-30 20:22:12 +0530

    Used State and Changed Components look
    state use කරල කෙලින්ම component එකක්(Text එකක්), button එකක් press කරලා page eka change or refresh නොවී change කරගන්නේ මේ විදියටයි....

--- App.js
+++ App.js
@@ -1,11 +1,59 @@
 import { StatusBar } from "expo-status-bar";
+import { useState } from "react";
+import { Alert } from "react-native";
+import { Pressable } from "react-native";
+import { SafeAreaView } from "react-native";
 import { StyleSheet,Text,View } from "react-native";
 
 function App(){
 
+  const[getContent,setContent]= useState("Content");
+
+const ui = (
+  <SafeAreaView style={styles.view1}>
+    <StatusBar hidden={false}/>
+
+    <Pressable onPress={m}>
+      <Text style={styles.text1}>Press</Text>
+    </Pressable>
+
+    <View>
+      <Text style={styles.text2}>{getContent}</Text>
+    </View>
 
 
+
+  </SafeAreaView>
+);
+
+return ui;
+
+function m(){
+  Alert.alert("Message","Content Changed");
+  setContent("niyamai");
+}
 
 }
+
+
+const styles = StyleSheet.create(
+  {
+    view1:{
+      flex:1,
+      justifyContent:"center",
+      alignItems:"center",
+    },
+    text1:{
+      fontSize:29,
+      color:"rgb(82, 92, 232)",
+    },
+    text2:{
+      fontSize:30,
+      color:"#0000ff",
+      marginTop:20,
+    }
+    
+  }
+);
 
 export default App;
