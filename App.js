ddc1798f8c95eb05de0ed4d03e1c09a05aa27e1a
mac_knight141 <123813453+isharax9@users.noreply.github.com>
2023-09-05 23:27:02 +0530

    2023-07-23 day mid commit
    03.20

--- App.js
+++ App.js
@@ -1,69 +1,84 @@
 import { StatusBar } from "expo-status-bar";
 import { useState } from "react";
-import { TouchableHighlight } from "react-native";
-import { Alert } from "react-native";
+import {
+  Alert,
+  Image,
+  TextInput,
+  TouchableHighlight,
+  TouchableOpacity,
+  TouchableWithoutFeedback,
+} from "react-native";
+import { Pressable } from "react-native";
 import { SafeAreaView } from "react-native";
-import { StyleSheet,Text,View } from "react-native";
+import { StyleSheet, Text, View } from "react-native";
 
-function App()
+function App() {
+  const [getContent, setContent] = useState("Content here");
 
-  const[getContent,setContent]= useState("Content");
+  const ui = (
+    <SafeAreaView style={styles.view1}>
+      <StatusBar hidden={false} />
 
-const ui = (
-  <SafeAreaView style={styles.view1}>
-    <StatusBar hidden={false}/>
+      <TextInput
+        style={{
+          height: 40,
+          width: 200,
+          marginBottom: 20,
+          borderWidth: 1,
+          padding: 5,
+          borderColor: "#655aff"
+        }}
+      />
+      <Pressable onPress={m}>
+        <View style={styles.btn}>
+          <Text style={styles.text1}>Press</Text>
+          <Image
+            source={{
+              uri: "https://cdn-icons-png.flaticon.com/128/9417/9417126.png",
+            }}
+            style={{ width: 25, height: 25 }}
+          />
+        </View>
+      </Pressable>
 
-    <TouchableHighlight onPress={m} activeOpacity={0.85}>
-      <View style={styles.btn}>
-        <Text style={styles.text1}>Press</Text>
+      <View>
+        <Text style={styles.text2}>{getContent}</Text>
       </View>
-    </TouchableHighlight>
+    </SafeAreaView>
+  );
 
-    <View>
-      <Text style={styles.text2}>{getContent}</Text>
-    </View>
+  function m() {
+    Alert.alert("Message", "Content Changed");
+    setContent("නියමයි...");
+  }
 
-
-
-  </SafeAreaView>
-);
-
-return ui;
-
-function m(){
-  Alert.alert("Message","Content Changed");
-  setContent("niyamai");
+  return ui;
 }
 
-
-// ai meka mehema uncolor wela tiyenne kiyala hoyanna.
-
-
-const styles = StyleSheet.create(
-  {
-    view1:{
-      flex:1,
-      justifyContent:"center",
-      alignItems:"center",
-    },
-    text1:{
-      fontSize:29,
-      color:"#ffffff",
-    },
-    text2:{
-      fontSize:30,
-      color:"#0000ff",
-      marginTop:20,
-    },
-    btn:{
-      backgroundColor: "#000000",
-      paddingHorizontal:20,
-      paddingVertical:10,
-      borderRadius:12,
-      
-    }
-    
-  }
-);
+const styles = StyleSheet.create({
+  view1: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  text1: {
+    fontSize: 20,
+    color: "rgb(82, 92, 232)",
+    paddingLeft: 10,
+    justifyContent: "center",
+  },
+  text2: {
+    fontSize: 20,
+    color: "#0000ff",
+    marginTop: 20,
+  },
+  btn: {
+    backgroundColor: "#40c7ff",
+    paddingHorizontal: 50,
+    paddingVertical: 10,
+    borderRadius: 10,
+    flexDirection: "row",
+  },
+});
 
 export default App;
